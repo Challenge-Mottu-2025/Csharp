890eb7074f5fb3dab4f7fd79ace90ca5abf96b45
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-08-18 09:36:44 -0300

    Corrigindo falta de chaves.

--- Domain/Filial.cs
+++ Domain/Filial.cs
@@ -1,14 +1,14 @@
 using challenge_dotnet.Domain.MotoRepository;
 
 namespace challenge_dotnet.Domain
-
-public class Filial
 {
-    public Guid Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Endereco { get; set; } = string.Empty;
-    public double TamanhoPatio { get; set; }
+    public class Filial
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+        public double TamanhoPatio { get; set; }
 
-    public ICollection<Moto> Motos { get; set; } = new List<Moto>();
-
+        public ICollection<Moto> Motos { get; set; } = new List<Moto>();
+    }
 }
--- Domain/LocalizacaoPatio.cs
+++ Domain/LocalizacaoPatio.cs
@@ -1,14 +1,14 @@
 using challenge_dotnet.Domain.MotoRepository;
 
 namespace challenge_dotnet.Domain
-
-public class LocalizacaoPatio
 {
-    public Guid Id { get; set; }
-    public int PosicaoX { get; set; }
-    public int PosicaoY { get; set; }
-    public string Descricao { get; set; } = string.Empty;
+    public class LocalizacaoPatio
+    {
+        public Guid Id { get; set; }
+        public int PosicaoX { get; set; }
+        public int PosicaoY { get; set; }
+        public string Descricao { get; set; } = string.Empty;
 
-    public ICollection<Moto> Motos { get; set; } = new List<Moto>();
-
+        public ICollection<Moto> Motos { get; set; } = new List<Moto>();
+    }
 }
